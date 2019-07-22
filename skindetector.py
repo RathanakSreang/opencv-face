f4c948824bc35c5fab78065e51c23d83aad3f5ef
Sreang Rathanak <sreangrathanak@yahoo.com>
2019-07-22 23:18:32 +0700

    My commit

--- skindetector.py
+++ skindetector.py
@@ -1,13 +1,6 @@
 import imutils
 import numpy as np
-import argparse
 import cv2
-
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-  help = "path to the (optional) video file")
-args = vars(ap.parse_args())
 
 # define the upper and lower boundaries of the HSV pixel
 # intensities to be considered 'skin'
@@ -16,15 +9,7 @@ upper = np.array([20, 255, 255], dtype = "uint8")
 min_YCrCb = np.array([0,133,77],np.uint8)
 max_YCrCb = np.array([235,173,127],np.uint8)
 
-# if a video path was not supplied, grab the reference
-# to the gray
-if not args.get("video", False):
-  camera = cv2.VideoCapture(0)
-
-# otherwise, load the video
-else:
-  camera = cv2.VideoCapture(args["video"])
-
+camera = cv2.VideoCapture(0)
 # keep looping over the frames in the video
 while True:
   # grab the current frame
@@ -32,40 +17,21 @@ while True:
 
   # if we are viewing a video and we did not grab a
   # frame, then we have reached the end of the video
-  if args.get("video") and not grabbed:
+  if not grabbed:
     break
 
-  # # resize the frame, convert it to the HSV color space,
-  # # and determine the HSV pixel intensities that fall into
-  # # the speicifed upper and lower boundaries
-  # frame = imutils.resize(frame, width = 400)
-  # converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-  # skinMask = cv2.inRange(converted, lower, upper)
-
-  # # apply a series of erosions and dilations to the mask
-  # # using an elliptical kernel
-  # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
-  # skinMask = cv2.erode(skinMask, kernel, iterations = 2)
-  # skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
-
-  # # blur the mask to help remove noise, then apply the
-  # # mask to the frame
-  # skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
-  # skin = cv2.bitwise_and(frame, frame, mask = skinMask)
-
-  # # show the skin in the image along with the mask
-  # cv2.imshow("images", np.hstack([frame, skin]))
-
+  frame = imutils.resize(frame, height=600)
   imageYCrCb = cv2.cvtColor(frame,cv2.COLOR_BGR2YCR_CB)
   skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
 
   skinYCrCb = cv2.bitwise_and(frame, frame, mask = skinRegionYCrCb)
 
-  # cv2.imwrite("/assets/img/skin-detection/ycrcb.png", np.hstack([image,skinYCrCb]))
-  cv2.imshow("images", np.hstack([frame, skinYCrCb]))
+  cv2.imshow("images", frame)
+  cv2.imshow("images_skin", skinYCrCb)
 
   # if the 'q' key is pressed, stop the loop
-  if cv2.waitKey(1) & 0xFF == ord("q"):
+  key = cv2.waitKey(1) & 0xFF
+  if key == ord("q"):
     break
 
 # cleanup the camera and close any open windows
